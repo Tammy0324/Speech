a8b994a7750143fe395f0a25273a7088faf288f4
Tammy0324 <u102314@gmail.com>
2022-03-19 17:44:21 +0800

    頁面美化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:project/screens/exePage.dart';
+import 'package:project/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,172 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('主頁'),
-        ),
-        body: HomePage(),
+      title: '主頁',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => exePage())  //轉跳頁面
-                );
-              },
-              child: Text('練習'),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => quizPage()) //轉跳頁面
-                );
-              },
-              child: Text('測驗'),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => vocPage())  //轉跳頁面
-                );
-              },
-              child: Text('單字卡'),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => settingPage())  //轉跳頁面
-                );
-              },
-              child: Text('設定'),
-            ),
-          ],
-        )
-    );
-  }
-}
-
-//使exePage可以使用setState函式（暫無用）
-// class exePageState extends StatefulWidget {
-//   @override
-//   exePage createState() => exePage();
-// }
-
-class exePage extends StatelessWidget {
-  var article = "The teacher announces that there is a test. "; //範例文章
-  IconData micicon = Icons.mic_outlined;
-  AudioPlayer player = AudioPlayer();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('練習'),
-      ),
-      body:
-        Column(
-          children: <Widget>[
-            Text(article),
-            Row(
-              children: <Widget>[
-                ElevatedButton.icon(
-                  icon: Icon(Icons.headphones_outlined,),
-                  label: Text(""),
-                  onPressed: sound,
-                ),
-                ElevatedButton.icon(
-                  icon: Icon(micicon),
-                  label: Text(""),
-                  onPressed: () {
-                    print('Mic');
-                    // setState(() {  //控制錄音按鈕圖形切換
-                    //   if (micicon == Icons.mic_outlined)
-                    //     micicon = Icons.stop_circle_outlined;
-                    //   else
-                    //     micicon = Icons.mic_outlined;
-                    // });
-                  },
-                )
-               ],
-            )
-          ]
-        )
-    );
-  }
-  void sound() {
-    print('Speech');
-    player.play('voice/example.mp3');
-  }
-}
-
-class quizPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('測驗'),
-      ),
-      body: quizPageBody(),
-    );
-  }
-}
-
-class quizPageBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-    );
-  }
-}
-
-class vocPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('單字卡'),
-      ),
-      body: vocPageBody(),
-    );
-  }
-}
-
-class vocPageBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-    );
-  }
-}
-
-class settingPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('設定'),
-      ),
-      body: settingPageBody(),
-    );
-  }
-}
-
-class settingPageBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(title: '主頁'),
+        '/audio': (context) => AudioSession(arIndex: (ModalRoute.of(context)!.settings.arguments as int)),
+      },
     );
   }
 }
---
+++ lib/screens/exePage.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+//import 'package:project/models/audio_rec.dart';
+
+class AudioSession extends StatefulWidget {
+  final int arIndex;
+
+  // do we need key?
+  AudioSession({Key? key, required this.arIndex}) : super(key: key);
+  //AudioSession({required this.arIndex});
+
+  @override
+  _AudioSessionState createState() => _AudioSessionState();
+}
+
+class _AudioSessionState extends State<AudioSession> {
+  int _selectedBottomBarItemIndex = 0;
+
+  void _onBottomBarItemTapped(int index) {
+    setState(() {
+      _selectedBottomBarItemIndex = index;
+    });
+  }
+
+  var article = "The teacher announces that there is a test. "; //範例文章
+  IconData micicon = Icons.mic_outlined;
+  AudioPlayer player = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("練習"),
+          // automaticallyImplyLeading: false, // not display <- back btn
+        ),
+        body: Container(
+          child: Text(article),
+        ),
+        bottomNavigationBar: Container(
+          height: 110,
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.blueAccent,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.headphones_outlined),
+                      iconSize: 38,
+                      color: Colors.white,
+                      onPressed: play,
+                    ),
+                  ),
+                  Text(
+                    'Play',
+                    style: TextStyle(color: Colors.blueAccent.withOpacity(0.8)),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.blueAccent,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.record_voice_over),
+                      iconSize: 38,
+                      color: Colors.white,
+                      onPressed: () {
+                        _onBottomBarItemTapped(1);
+                      },
+                    ),
+                  ),
+                  Text(
+                    'Record',
+                    style: TextStyle(color: Colors.blueAccent.withOpacity(0.8)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+    );
+  }
+  void play() {
+    print('Speech');
+    player.play('voice/example.mp3');
+  }
+}
