e119c6b45317d0fd395bc166bc2a979d9371e382
Tammy0324 <u102314@gmail.com>
2022-09-06 11:27:39 +0800

    依句子進行比對、下載上傳的錄音檔、暫時寫死隨機文章

--- fastapi-master/tests/main.py
+++ fastapi-master/tests/main.py
@@ -5,6 +5,8 @@ import random
 import shutil
 from fastapi.responses import FileResponse
 import nltk
+import azure.cognitiveservices.speech as speechsdk
+from difflib import *
 
 app = FastAPI()
 
@@ -12,15 +14,14 @@ url = "https://www.eslfast.com/kidsenglish/ke/ke{}{}{}.htm"
 
 a = random.choice('0')
 while True:
-    b = random.choice('1234567')
-    c = random.choice('1234567890')
+    b = random.choice('5')
+    c = random.choice('7')
     if int(b) * 10 + int(c) < 76:
         break
 
 r = requests.get(url.format(a, b, c))
 soup = BeautifulSoup(r.text, "html.parser")
 sel = soup.select('div.content-body div.contain font')
-
 
 # 處理文章內容
 contain = ""
@@ -32,17 +33,18 @@ for i in range(1, len(arr)):
     article += arr[i]
     article += " "
 print(a, b, c)
-print(article)
 print("")
 sen = nltk.sent_tokenize(article)
-print(sen)
 l = len(sen)
 print("l = ", l)
 sentence = "\n"
+list = []
 for i in sen:
+    sentence = ""
     sentence += i
+    list.append(sentence)
     sentence += "\n"
-print(sentence)
+print(list)
 
 filePath = "voice/{}{}{}/{}.mp3"
 
@@ -52,20 +54,41 @@ def create_item():
     return sentence
 
 
-@app.post("/recorder")
+@app.post("/recorder/{num}")
 async def root(file: UploadFile = File(...)):
-    return "123"
+    print("file_name", file.filename)
+    # 下載被上傳的音檔
+    with open(file.filename, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
 
 
-# @app.get("/download_file")
-# def download_file():
-#     return FileResponse(path=filePath.format(a, b, c, l - 1), filename=filePath.format(a, b, c, l - 1))
-
-
-for i in range(1, l+1):
+for i in range(1, l + 1):
     @app.get("/example/{example_num}")
     async def example(example_num):
         print(example_num)
         print(a, b, c)
         print(filePath.format(a, b, c, example_num))
         return FileResponse(filePath.format(a, b, c, example_num))
+
+
+@app.get('/result/{num}')
+async def _result(num: int):
+    # return accuracy,fluency,complete,pron
+    file = "Audio{}.wav"
+    speech_config = speechsdk.SpeechConfig(subscription="b751009eb5f545f8a4d0ce3cab8d7c71", region="eastasia")
+    audio_config = speechsdk.audio.AudioConfig(filename=file.format(num))
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    sen = speech_recognizer.recognize_once_async().get()
+    result = sen.text
+    print("result = ", result)
+    ratio = match(result, num)
+    return result, ratio
+
+
+# sen為錄音檔中的文字內容，num為錄音檔對應到的句子編號
+def match(sen, num):
+    ans = list[num-1]
+    ratio = SequenceMatcher(None, sen, ans).ratio()
+    print("ans = ", ans)
+    print("sen = ", sen)
+    return ratio
