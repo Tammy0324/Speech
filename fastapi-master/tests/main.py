d0e77f3d1a1d26356b2ccf86b05f62ef8b64bbe4
Tammy0324 <u102314@gmail.com>
2022-11-08 10:08:55 +0800

    修正辨識時答案獲取錯誤 優化辨識分數

--- fastapi-master/tests/main.py
+++ fastapi-master/tests/main.py
@@ -17,9 +17,9 @@ list = []
 def ran():
     a = random.choice('0')
     while True:
-        b = random.choice('0123456789')
-        c = random.choice('0123456789')
-        if int(b) * 10 + int(c) < 76:
+        b = random.choice('1234567890')
+        c = random.choice('1234567890')
+        if (int(b) * 10 + int(c) < 76) | (int(b) * 10 + int(c) == 0):
             break
     sum = a + b + c
     dic = {'article_num': sum}
@@ -102,8 +102,9 @@ async def _result(num):
     sen = speech_recognizer.recognize_once_async().get()
     result = sen.text
     print("result = ", result)
-    ratio = '{:.2f}%'.format(match(result, num))
-    return ratio
+    ratio = match(result, num)
+    score = str(ratio) + '%'
+    return score
 
 
 # sen為錄音檔中的文字內容，num為錄音檔對應到的句子編號
@@ -113,46 +114,8 @@ def match(sen, num):
     index = int(num) - 1
     ans = list[index]
     ratio = SequenceMatcher(None, sen, ans).ratio()
+    score = round(ratio * 100, 2)
     print("ans = ", ans)
     print("sen = ", sen)
-    return ratio
-
-# def test(n: int):
-#     if n == 0:
-#         dic = {'article_num': 0, 'len': 0, 'sen': 0}
-#         article.update(dic)
-#     else:
-#         url = "https://www.eslfast.com/kidsenglish/ke/ke{}.htm"
-#         r = requests.get(url.format(article['article_num']))
-#         soup = BeautifulSoup(r.text, "html.parser")
-#         sel = soup.select('div.content-body div.contain font')
-#
-#         # 處理文章內容
-#         contain = ""
-#         text = ""
-#         for s in sel:
-#             contain = s.text
-#         arr = contain.split('\n')
-#         for i in range(1, len(arr)):
-#             text += arr[i]
-#             text += " "
-#         print(article['article_num'])
-#         print("")
-#         sen = nltk.sent_tokenize(article)
-#         l = len(sen)
-#         print("l = ", l)
-#         sentence = "\n"
-#         list = []
-#         for i in sen:
-#             temp = ""
-#             sentence += i
-#             temp += i
-#             list.append(temp)
-#             sentence += "\n"
-#         dic = {'article_num': 1, 'len': l, 'sen': sentence}
-#         article.update(dic)
-#
-#
-# article = {}
-# test(0)
-# print(article)
+    print("score : ", score)
+    return score
